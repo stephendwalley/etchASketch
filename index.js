2203e03171a63918d9052690819d3c5cdc07e8ad
stephendwalley <76471970+stephendwalley@users.noreply.github.com>
2021-01-20 17:55:50 +1100

    Fixed color classes

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ let container = document.querySelector("#container");
 
 for (i = 0; i < 256; i++) {
 	let newGrid = document.createElement("div");
-	newGrid.classList.add("grid", "uncoloured");
+	newGrid.classList.add("grid-uncoloured");
 	container.appendChild(newGrid);
 	newGrid.addEventListener("mouseover", adjustGridColor);
 	console.log("new grid made");
 }
 
 function adjustGridColor(e) {
-    let newGrid = document.querySelectorAll(".grid");
-    this.className = "grid";
-	this.className = "coloured", "grid";
+	let newGrid = document.querySelectorAll(".grid-uncoloured");
+	this.className = "grid-coloured";
 	console.log("adjustGrid Color!");
 }
 
-let button = document.querySelector(".reset")
+let button = document.querySelector(".reset");
 button.addEventListener("click", resetGridColor);
 
-
 function resetGridColor(e) {
-    console.log("reset")
-    let newGrid = document.querySelectorAll(".grid");
-    newGrid.classList.remove("coloured");
+	console.log("reset");
+	let newGrid = document.querySelectorAll(".grid");
+	newGrid.classList.remove("coloured");
 }
