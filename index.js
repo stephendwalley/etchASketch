955d1b6c7e56d15371f7b3ff0b1223b9d2d2424c
stephendwalley <76471970+stephendwalley@users.noreply.github.com>
2021-01-20 17:54:00 +1100

    Changes class

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ let container = document.querySelector("#container");
 
 for (i = 0; i < 256; i++) {
 	let newGrid = document.createElement("div");
-	newGrid.classList.add("grid");
+	newGrid.classList.add("grid", "uncoloured");
 	container.appendChild(newGrid);
 	newGrid.addEventListener("mouseover", adjustGridColor);
 	console.log("new grid made");
 }
 
-
 function adjustGridColor(e) {
-	let newGrid = document.querySelectorAll(".grid");
-    this.removeAttribute("class");
-    
-    // checks what class is applied and applys a different class to change the suitable color
-
+    let newGrid = document.querySelectorAll(".grid");
+    this.className = "grid";
+	this.className = "coloured", "grid";
 	console.log("adjustGrid Color!");
+}
+
+let button = document.querySelector(".reset")
+button.addEventListener("click", resetGridColor);
+
+
+function resetGridColor(e) {
+    console.log("reset")
+    let newGrid = document.querySelectorAll(".grid");
+    newGrid.classList.remove("coloured");
 }
