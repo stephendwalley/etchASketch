3aa603e190beafee77bc8f9d4396dbbfc6dc9ba4
stephendwalley <76471970+stephendwalley@users.noreply.github.com>
2021-01-21 19:20:05 +1100

    Working reset button and fixed size grid

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 let container = document.querySelector("#container");
-let newGrid = document.querySelectorAll(".grid-uncoloured");
 
 for (i = 0; i < 256; i++) {
 	let newGrid = document.createElement("div");
-	newGrid.classList.add("grid-uncoloured");
+	newGrid.classList.add("grid-uncolored");
 	container.appendChild(newGrid);
 	newGrid.addEventListener("mouseover", adjustGridColor);
 	console.log("new grid made");
 }
 
 function adjustGridColor(e) {
-	let newGrid = document.querySelectorAll(".grid-uncoloured");
-	this.className = "grid-coloured";
+	this.className = "grid-colored";
 	console.log("adjustGrid Color!");
 }
 
@@ -19,6 +17,10 @@ let button = document.querySelector(".reset");
 button.addEventListener("click", resetGridColor);
 
 function resetGridColor(e) {
-	console.log("reset");
-	newGrid.className = "grid-uncoloured";
+	let coloredGrid = document.querySelectorAll(".grid-colored");
+	coloredGrid.forEach(element => {
+		element.className = "grid-uncolored";
+		console.log("reset");
+	});
+	
 }
